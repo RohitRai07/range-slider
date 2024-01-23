564e0e1611f300bca61d3c95f44b2b94de5644fe
Rohit Rai <66778858+RohitRai07@users.noreply.github.com>
2024-01-23 19:07:38 +0530

    message in script

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// if you want dynamic just uncomment the js & all commented part even in css
+
+
 // const icon = document.querySelector("#icon"),
 //        range = document.querySelector("input"),
 //        slideValue = document.querySelector(".slide-value");
