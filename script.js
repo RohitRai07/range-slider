0a2b14778c00b945944d3aba58552347930c59c4
Rohit Rai <66778858+RohitRai07@users.noreply.github.com>
2024-01-23 19:05:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// const icon = document.querySelector("#icon"),
+//        range = document.querySelector("input"),
+//        slideValue = document.querySelector(".slide-value");
+
+//      range.addEventListener("input", () => {
+//        let rangeVal = range.value;
+//        slideValue.innerText = rangeVal;
